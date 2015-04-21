afc7dea3c221975a3864bf5c3dac2bec41657732
edggarv <evasquez@interac.cl>
2015-04-21 17:13:40 -0400

    anunciodao y logica anuncio

---
+++ src/main/java/cl/interac/dao/AnuncioDAO.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.interac.dao;
+
+import cl.interac.Entidades.Anuncio;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import cl.interac.Entidades.Usuario;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Yesenia Doria L.
+ */
+@Repository
+public class AnuncioDAO {
+    @PersistenceContext
+    private EntityManager em;
+    
+    public List<Anuncio> obtenerTodos() {
+        return em.createNamedQuery("Anuncio.findAll").getResultList();
+    }
+    public void guardar(Anuncio a){
+        if (a.getIdAnuncio()== null) em.persist(a);
+        else em.merge(a);
+    }
+    public void eliminarAnuncio(Anuncio a) {
+        Anuncio Anuncio = em.find(Anuncio.class, a.getIdAnuncio());
+        em.remove(Anuncio);
+    }
+    
+}
---
+++ src/main/java/cl/interac/negocio/LogicaAnuncio.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.interac.negocio;
+
+
+import cl.interac.dao.AnuncioDAO;
+import cl.interac.Entidades.Anuncio;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author edggar
+ */
+@Component
+@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class LogicaAnuncio {
+    @Autowired
+     private AnuncioDAO anuncioDAO;
+     
+     @Transactional(readOnly = true)
+     public List<Anuncio> obtenerTodos() {
+        return anuncioDAO.obtenerTodos();
+    }
+    @Transactional(readOnly = false)
+    public void guardarAnuncio(Anuncio a){
+        anuncioDAO.guardar(a);
+    }
+    @Transactional(readOnly = false)
+    public void eliminarAnuncio(Anuncio a){
+        anuncioDAO.eliminarAnuncio(a);
+    }
+}
