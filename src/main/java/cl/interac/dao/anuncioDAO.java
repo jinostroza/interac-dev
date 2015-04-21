c9ec51c7beba7b959527962714cd28d8a7dcfc7e
lbelmar <lbelmar@interac.cl>
2015-04-21 15:07:32 -0400

    un mensaje

---
+++ src/main/java/cl/interac/dao/anuncioDAO.java
@@ -0,0 +1,42 @@
+package cl.interac.dao;
+
+import cl.interac.Entidades.Anuncio;
+import cl.interac.Entidades.Usuario;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+/**
+ * Created by luis on 21-04-2015.
+ */
+@Repository
+
+public class anuncioDAO {
+
+
+    private EntityManager em;
+
+    public List<Anuncio> obtenerTodos() {  return em.createNamedQuery("Anuncio.findAll").getResultList();    }
+
+
+
+    public void guardar(Anuncio n) {
+        if (n.getIdAnuncio() == null) em.persist(n);
+        else em.merge(n);
+    }
+    public void eliminarAnuncio(Anuncio n) {
+     Anuncio nun = em.find(Anuncio.class, n.getIdAnuncio());
+        em.remove(nun);
+    }
+
+    /*public Anuncio obtenerAnuncio(String anuncio ) {
+        try {
+            return (Anuncio) em.createNamedQuery("Anuncio.findByDescAnuncio")
+                     .setParameter("DescAnuncio", anuncio)
+                    .getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
+    }*/
+
+}
