8a2f426b44f75cf2e0c6b558d83f4cc6aca5317b
jinostroza <jorge.inostroza.u@gmail.com>
2015-04-25 12:55:59 -0300

    arreglo mantenedor anuncio

---
+++ src/main/java/cl/interac/presentacion/anuncios/MantenedorAnuncios.java
@@ -0,0 +1,18 @@
+package cl.interac.presentacion.anuncios;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+/**
+ * Created by Joaco on 24-04-2015.
+ */
+@Component
+@Scope("flow")
+public class MantenedorAnuncios implements Serializable
+{
+
+    public void inicio() {
+    }
+}
