149f897a200d0d3ae0eeab011f91598ff0b83583
joacoch <jchacon@interac.cl>
2015-04-29 11:15:20 -0400

    "Cambios Campana"

--- src/main/java/cl/interac/presentacion/campanas/MantenedorCampanas.java
+++ src/main/java/cl/interac/presentacion/campanas/MantenedorCampanas.java
@@ -1,10 +1,14 @@
 package cl.interac.presentacion.campanas;
 
+import cl.interac.entidades.Campana;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.Serializable;
+import java.util.List;
 
+import cl.interac.negocio.LogicaCampana;
 /**
  * Created by Joaco on 24-04-2015.
  */
@@ -12,7 +16,30 @@ import java.io.Serializable;
 @Scope("flow")
 public class MantenedorCampanas implements Serializable
 {
+    public Campana tmpCampana;
+    @Autowired
+    private LogicaCampana logicaCampana;
+
+    private List<Campana> campanas;
 
     public void inicio() {
+        tmpCampana = new Campana();
+    }
+
+    public List<Campana> getCampanas() {
+        return campanas;
+    }
+
+    public void setCampanas(List<Campana> campanas) {
+        this.campanas = campanas;
+    }
+
+    public void CrearCampana()
+    {
+        if (tmpCampana!=null)
+        {
+            logicaCampana.guardarCampana(tmpCampana);
+        }
+        tmpCampana = new Campana();
     }
 }
