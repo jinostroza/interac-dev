edd0e81b67409d88e05ef51d823a432c0dcf47a9
edggarv <evasquez@interac.cl>
2015-04-29 17:34:15 -0400

    categoria e totem

---
+++ src/main/java/cl/interac/presentacion/categorias/MantenedorCategoria.java
@@ -0,0 +1,37 @@
+package cl.interac.presentacion.categorias;
+
+import cl.interac.entidades.Categoria;
+import cl.interac.negocio.LogicaCategoria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by Joaco on 24-04-2015.
+ */
+@Component
+@Scope("flow")
+public class MantenedorCategoria implements Serializable
+{
+    public Categoria categoria;
+    @Autowired
+    private LogicaCategoria logicaCategoria;
+
+    private List<Categoria> categorias;
+
+    public void inicio() {
+        categoria = new Categoria();
+    }
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
+    }
+
+}
---
+++ src/main/java/cl/interac/presentacion/totems/MantenedorTotems.java
@@ -0,0 +1,37 @@
+package cl.interac.presentacion.totems;
+
+import cl.interac.entidades.Totem;
+import cl.interac.negocio.LogicaTotem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by Joaco on 24-04-2015.
+ */
+@Component
+@Scope("flow")
+public class MantenedorTotems implements Serializable
+{
+    public Totem totem;
+    @Autowired
+    private LogicaTotem logicaTotem;
+
+    private List<Totem> totems;
+
+    public void inicio() {
+        totem = new Totem();
+    }
+
+    public List<Totem> getTotems() {
+        return totems;
+    }
+
+    public void setTotems(List<Totem> totems) {
+        this.totems = totems;
+    }
+
+}
