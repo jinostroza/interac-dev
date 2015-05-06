f174c644ac5d516060cef4c84fa3e94f946a1569
lbelmar <lbelmar@interac.cl>
2015-05-06 11:09:25 -0400

    Se agrega UbicacionDAO y Se agrega query a Ubicacion, commit temporal para revisión de persistencia

---
+++ src/main/java/cl/interac/dao/UbicacionDAO.java
@@ -0,0 +1,35 @@
+package cl.interac.dao;
+
+import cl.interac.entidades.Ubicacion;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by luis on 27-04-2015.
+ */
+@Repository
+public class UbicacionDAO  {
+  @PersistenceContext
+    private EntityManager em;
+    public List<Ubicacion> ObtenerTodos(){return em.createNamedQuery("Ubicacion.findAll").getResultList();}
+
+    public void guardar (Ubicacion b){
+        if(b.getIdubicacion()== null) em.persist(b);
+
+        else em.merge(b);
+    }
+
+   public void eliminarUbicacion(Ubicacion b){
+       Ubicacion ubicacion = em.find(Ubicacion.class, b.getIdubicacion());
+       em.remove(ubicacion);
+
+   }
+
+
+
+
+}
