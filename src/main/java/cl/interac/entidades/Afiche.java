579317d1d6d6842a7424bcd4113cfd2e2226c626
lbelmar <lbelmar@interac.cl>
2015-05-29 14:26:48 -0400

    mejoras y el modulo afiche(para las id y los path de las imagenes 2

---
+++ src/main/java/cl/interac/entidades/Afiche.java
@@ -0,0 +1,53 @@
+package cl.interac.entidades;
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+/**
+ * Created by luis on 27-05-2015.
+ */
+
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "afiche.findAll", query = "select a from Afiche a")
+
+})
+public class Afiche implements Serializable {
+    private Integer idAfiche;
+    private String path;
+
+    //relaciones
+    private Anuncio anuncio;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idAfiche", nullable = false, updatable = false, insertable = true)
+
+    public Integer getIdAfiche() {
+        return idAfiche;
+    }
+
+    public void setIdAfiche(Integer idAfiche) {
+        this.idAfiche = idAfiche;
+    }
+
+    @Basic
+    @Column(name = "path", nullable = true, updatable = true, insertable = true)
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+
+    @OneToOne(mappedBy = "afiche")
+    public Anuncio getAnuncio() {
+        return anuncio;
+    }
+
+    public void setAnuncio(Anuncio anuncio) {
+        this.anuncio = anuncio;
+    }
+}
---
+++ src/main/java/cl/interac/presentacion/mantenedorAfiches.java
@@ -0,0 +1,7 @@
+package cl.interac.presentacion;
+
+/**
+ * Created by luis on 28-05-2015.
+ */
+public class mantenedorAfiches {
+}
