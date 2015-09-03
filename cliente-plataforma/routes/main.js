ff88641b87b0273a75645b49d533c86ee03f3b94
Claudio Olivares <claudio.pol.olivares@gmail.com>
2015-09-02 23:23:05 -0300

    Se implementa version alpha del slider, pero funcional

--- cliente-plataforma/routes/main.js
+++ cliente-plataforma/routes/main.js
@@ -1,4 +1,6 @@
 
+var FileSystem = require("fs");
+
 var enrutador = function(App) {
     // la raiz del sistema
     App.get("/", function(req, res) {
@@ -7,7 +9,19 @@ var enrutador = function(App) {
         });
     });
 
-    App.get()
+    App.get("/getMedia", function(req, res) {
+        FileSystem.readdir(req.appPath + "/public/media/", function(error, files){
+            var media = [];
+
+            if (error) res.send(JSON.stringify(media));
+
+            for (var i = files.length - 1; i >= 0; i--) {
+                media.push("/public/media/"+files[i]);
+            };
+
+            res.send(JSON.stringify(media));
+        });
+    });
 };
 
 module.exports = enrutador;
