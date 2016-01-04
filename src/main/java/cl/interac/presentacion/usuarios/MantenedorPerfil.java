36ebb7eef6d2108c16becd91337ac12b8d504140
PPablo <pe.pastene@gmail.com>
2016-01-04 16:25:29 -0300

    Se logra confirmar y posteriormente cambiar la contraseña en el mantenedor de perfil y encriptada a md5. Hay problemas a nivel de vista, pero funciona.

--- src/main/java/cl/interac/presentacion/usuarios/MantenedorPerfil.java
+++ src/main/java/cl/interac/presentacion/usuarios/MantenedorPerfil.java
@@ -17,6 +17,9 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.Serializable;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 /**
@@ -51,12 +54,12 @@ public class MantenedorPerfil implements Serializable {
 
 
        public void cambiaPerfil() {
-
         if (!claveConfirmada.equals(claveNueva)) {
             FacesUtil.mostrarMensajeError("Operación fallida", "La nueva contraseña no coincide con lo confirmado");
             return;
-        }else{
-            logicaUsuario.cambiarClave(userSession.getUsuario().getUsername(), claveConfirmada);
+        }
+        else{
+            logicaUsuario.cambiarClave(userSession.getUsuario().getUsername(), md5(claveConfirmada));
             FacesUtil.mostrarMensajeInformativo("Operación Exitosa", "Se ha cambiado correctamente la contraseña");
         }
 
@@ -69,7 +72,28 @@ public class MantenedorPerfil implements Serializable {
         FacesUtil.mostrarMensajeInformativo("Operación exitosa", "usuario [" + userSession.getUsuario().getUsername() + "] modificado");
     }
 
+    public static String md5(String input){
 
+        String md5 = null; //La variable esta vacia
+        if(null == input){ //Si es nulo el input, la funcion retornara null
+            return null;
+        }
+
+        try{
+            //Create MessageDigest object for MD5
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+
+            //Update input string in message digest
+            digest.update(input.getBytes(), 0, input.length());
+
+            //Converts message digest value in base 16 (hex)
+            md5 = new BigInteger(1, digest.digest()).toString(16);
+        }
+        catch(NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return md5;
+    }
 
     //getter and setter
 
@@ -137,9 +161,7 @@ public class MantenedorPerfil implements Serializable {
         this.claveNueva = claveNueva;
     }
 
-    public String getClaveConfirmada() {
-        return claveConfirmada;
-    }
+    public String getClaveConfirmada() { return claveConfirmada; }
 
     public void setClaveConfirmada(String claveConfirmada) {
         this.claveConfirmada = claveConfirmada;
